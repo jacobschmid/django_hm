e0888dffea7acb9bd505253b9f4250e5f92f98cf
Jacob Schmid <jacobschmid@comcast.net>
2015-02-06 16:43:54 -0700

    This is a new commit for django on hm

---
+++ djangosite/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'djangosite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':'/home2/jacobsc1/djangosite/static'}),
+)
